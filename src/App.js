3b5f927b87a115e57c8d3121bae0f15b1f641b1f
mwizerwa77 <mwizerwa77@gmail.com>
2019-06-15 23:50:10 +0200

    deploying to heroku

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi Docker is cool!
+        Docker app deployed on heroku
         </p>
         
       </header>
